4325e3e045281829a75aa0e045f92f70aa57281d
Sina Xhpm <harvast68@gmail.com>
2017-03-02 12:30:29 +0330

    Create sinchi.sh

---
+++ sinchi.sh
@@ -0,0 +1,35 @@
+
+sudo apt-get update
+
+sudo apt-get upgrade
+
+sudo apt-get install libreadline-dev libconfig-dev libssl-dev lua5.2 liblua5.2-dev lua-socket lua-sec lua-expat libevent-dev make unzip git redis-server autoconf g++ libjansson-dev libpython-dev expat libexpat1-dev
+
+sudo apt-get install lua-lgi
+
+sudo apt-get install software-properties-common
+
+sudo add-apt-repository ppa:ubuntu-toolchain-r/test 
+
+sudo apt-get install libstdc++6
+
+sudo apt-get update
+
+sudo apt-get upgrade
+
+sudo apt-get dist-upgrade
+
+wget http://luarocks.org/releases/luarocks-2.2.2.tar.gz
+ tar zxpf luarocks-2.2.2.tar.gz
+ cd luarocks-2.2.2
+ ./configure; sudo make bootstrap
+ sudo luarocks install luasocket
+ sudo luarocks install luasec
+ sudo luarocks install redis-lua
+ sudo luarocks install lua-term
+ sudo luarocks install serpent
+ sudo luarocks install dkjson
+ sudo luarocks install lanes
+ sudo luarocks install Lua-cURL
+echo Was successfully installed
+echo Run ./launch.sh
