16ec0657f2c6a17a2886f677085348f54fe871a1
Sina Xhpm <harvast68@gmail.com>
2017-03-29 20:09:54 +0430

    Update sinchi.py

--- sinchi.py
+++ sinchi.py
@@ -5,8 +5,12 @@ import os
 redis = redis.Redis('localhost')
 id = input("Enter SinChi Number (1,2,3,4,5,...) : ")
 source = os.popen("cat ./bot.lua").read()
-launcher = """while true; do
+launcher = """#!/bin/bash 
+COUNTER=0
+while [  $COUNTER -lt 5 ]; do
+kill $(pgrep telegram-cli)
   ./tg -p sinchi-{} -s bot.lua
+sleep 0.1
 done""".format(id,id)
 newlauncher = open("sinchi-{}.sh".format(id),"w")
 newlauncher.write(launcher)
