0f7aa4fc9a9e08b47e383efd61436d61e90a548a
Sina Xhpm <harvast68@gmail.com>
2017-04-04 14:52:56 +0430

    Update cleaner.sh

--- cleaner.sh
+++ cleaner.sh
@@ -3,15 +3,7 @@ while true ; do
   for entr in sinchi-*.sh ; do
     entry="${entr/.sh/}"
     ./off.sh
-    rm -rf ~/.telegram-cli/$entry/data/animation/*
-    rm -rf ~/.telegram-cli/$entry/data/audio/*
-    rm -rf ~/.telegram-cli/$entry/data/document/*
-    rm -rf ~/.telegram-cli/$entry/data/photo/*
-    rm -rf ~/.telegram-cli/$entry/data/sticker/*
-    rm -rf ~/.telegram-cli/$entry/data/temp/*
-    rm -rf ~/.telegram-cli/$entry/data/video/*
-    rm -rf ~/.telegram-cli/$entry/data/voice/*
-    rm -rf ~/.telegram-cli/$entry/data/profile_photo/*
+    rm -rf ~/.telegram-cli/$entry/data/*
     ./on.sh
   done
   echo All SinChi is Running!
