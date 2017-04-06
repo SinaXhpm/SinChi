b89f03f8c28a90f8bbc368c6248ca06241d478a4
Sina Xhpm <harvast68@gmail.com>
2017-04-06 17:53:11 +0430

    Update on.sh

--- on.sh
+++ on.sh
@@ -1,5 +1,20 @@
+#!/bin/bash
+while true ; do
 for entr in sinchi-*.sh ; do
     entry="${entr/.sh/}"
+    killall tmux
+    rm -rf ~/.telegram-cli/$entry/data/animation/*
+    rm -rf ~/.telegram-cli/$entry/data/audio/*
+    rm -rf ~/.telegram-cli/$entry/data/document/*
+    rm -rf ~/.telegram-cli/$entry/data/photo/*
+    rm -rf ~/.telegram-cli/$entry/data/sticker/*
+    rm -rf ~/.telegram-cli/$entry/data/temp/*
+    rm -rf ~/.telegram-cli/$entry/data/video/*
+    rm -rf ~/.telegram-cli/$entry/data/voice/*
+    rm -rf ~/.telegram-cli/$entry/data/profile_photo/*
+rm -rf ~/.telegram-cli/$entry/data/thumb/*
     tmux new-session -d -s $entry "./$entr"
   done
   echo 'On!'
+sleep 1080
+done
