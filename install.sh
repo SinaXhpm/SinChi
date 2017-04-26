c7d5367715d3f39bd7dac3171edba33e72638f4e
Sina Xhpm <harvast68@gmail.com>
2017-04-26 12:02:33 +0430

    Update install.sh

--- install.sh
+++ install.sh
@@ -12,9 +12,6 @@ white() {
 aa() {
  sudo apt-get install
 }
-bb() {
- sudo luarocks install
-}
 function logo1_SinChi() {
 green "     >>>>               Welcome to SinChi bot                         <<<<      "
 white "     >>>>             I wanna install it for you                      <<<<      "
