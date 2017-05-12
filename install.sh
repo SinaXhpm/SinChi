0a2835d3fd374ee5c29b8f9bf6b14dbb9b2144c4
Sina Xhpm <harvast68@gmail.com>
2017-05-12 20:34:32 +0430

    Update install.sh

--- install.sh
+++ install.sh
@@ -4,21 +4,13 @@ sudo apt-get update
 sudo apt-get upgrade
 
 sudo apt-get install libreadline-dev libconfig-dev libssl-dev lua5.2 liblua5.2-dev lua-socket lua-sec lua-expat libevent-dev make unzip git redis-server autoconf g++ libjansson-dev libpython-dev expat libexpat1-dev
-
 sudo apt-get install lua-lgi
-
 sudo apt-get install software-properties-common
-
 sudo add-apt-repository ppa:ubuntu-toolchain-r/test 
-
 sudo apt-get install libstdc++6
-
 sudo apt-get update
-
 sudo apt-get upgrade
-
 sudo apt-get dist-upgrade
-
 wget http://luarocks.org/releases/luarocks-2.2.2.tar.gz
  tar zxpf luarocks-2.2.2.tar.gz
  cd luarocks-2.2.2
